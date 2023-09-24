7ecff4e557ec44f316526dc7fcb38bb9f7ea2cd3
MahBadran93 <mahmoud.s.badran@gmail.com>
2023-09-24 16:31:28 +0300

    compete setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from setuptools import setup, find_packages
 
+def get_req(path:str):
+    """
+    return list of packages"
+    """
+
+    reqs = []
+
+    with open(path) as file_obj:
+        reqs = file_obj.readlines()
+        reqs = [req.replace("\n", "") for req in reqs]
+
+    return reqs
+
+
+path = './requirements.txt'
+
+
 setup(
     name='test_mlflow',
     version='0.0.1',
     author='mahmoud',
     author_email='mahmoud.s.badran@gmail.com',
-    description='mlops project structure (testing and deployment with CD/CI pipeline)'
+    description='mlops project structure (testing and deployment with CD/CI pipeline)',
+    packages=find_packages(),
+    install_requires=get_req(path)
 )
