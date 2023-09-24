4f37979bd13cf9adf0862e8fb6c8d8c9b4c69771
MahBadran93 <mahmoud.s.badran@gmail.com>
2023-09-24 16:04:32 +0300

    add setup file information

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test_mlflow',
+    version='0.0.1',
+    author='mahmoud',
+    author_email='mahmoud.s.badran@gmail.com',
+    description='mlops project structure (testing and deployment with CD/CI pipeline)'
+)
